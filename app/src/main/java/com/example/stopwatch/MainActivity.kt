681a8b4586cdcac536ad8364f5127823aee328bb
Buffer <khopark123@naver.com>
2020-01-01 22:02:08 +0900

    second commit

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.stopwatch
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
@@ -9,10 +10,22 @@ import kotlin.concurrent.timer
 class MainActivity : AppCompatActivity() {
     private var time = 0
     private var timerTask: Timer? = null
+    private var isRunning = false
+    private var lap = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab.setOnClickListener{
+            isRunning = !isRunning
+
+            if (isRunning){
+                start()
+            }else {
+                pause()
+            }
+        }
     }
 
     private fun start(){
@@ -28,5 +41,19 @@ class MainActivity : AppCompatActivity() {
                 milliTextView.text = "$milli"
             }
         }
+    }
+
+    private fun pause(){
+        fab.setImageResource(R.drawable.ic_play_arrow_black_24dp)
+        timerTask?.cancel()
+    }
+
+    private fun recordLapTime(){
+        val lapTime = this.time
+        val textView = TextView(this)
+        textView.text = "$lap Lap : ${lapTime / 100}.${lapTime % 100}"
+
+        lapLayout.addView(textView, 0)
+        lap++
     }
 }
